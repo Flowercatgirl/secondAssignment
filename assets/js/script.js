369349fd2756aa9c354b5ed5f47f20f1b69d43f3
Flowercatgirl <oksana.pankeviciene@gmail.com>
2024-05-06 13:00:34 +0100

    add weather api into script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
 console.log('Connected!');
+
+const apiKey ='d5f1b5feddd32a1168799f9ddb5091a7';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=vilnius';
+
+async function checkWeather(){
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    let data = await response.json();
+
+    console.log(data);
+}
+
+checkWeather();
