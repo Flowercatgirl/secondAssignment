2c998ae8fec69bd1959cb7424372b6c656894a28
Flowercatgirl <oksana.pankeviciene@gmail.com>
2024-06-25 23:46:21 +0100

    Get sunrise, sunset and current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,6 +24,34 @@ async function checkWeather(city) {
         document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
         document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h';
 
+        // This function gets City current time, using API response
+        function getDate(dt, timezone) {
+            /* First convert dt and timezone to integers.
+            dt represents the number of seconds since January 1st, 1970 UTC 
+            and timezone indicates by how many seconds the timezone in City is different from UTC
+            Adding these two integers gives the number of seconds from January 1st, 1970 to current time in City. 
+            To convert this into a date, we must use new Date(...). 
+            In JS, new Date() accepts only utc in milliseconds so we must convert utc_seconds to milliseconds.
+            new Date(utc_milliseconds) gives the current time in City in my local timezone 
+            which is different from the timezone in City.
+            new Date(utc_milliseconds).toUTCString() changes the timezone accordingly. */
+            const utc_seconds = parseInt(dt, 10) + parseInt(timezone, 10);
+            const utc_milliseconds = utc_seconds * 1000;
+            const local_date = new Date(utc_milliseconds).toUTCString();
+            return local_date;
+          }
+
+        const cityTime = getDate(data.dt, data.timezone);
+        const citySunrise_int = new Date((data.sys.sunrise + data.timezone) * 1000);
+        const citySunrise = citySunrise_int.toString();
+        const citySunset = new Date((data.sys.sunset + data.timezone) * 1000).toString();
+
+        let c_sunrise = citySunrise.slice(16, 24);
+        let c_sunset = citySunset.slice(16, 24);
+
+        console.log(c_sunrise + '   '+c_sunset);
+        console.log(cityTime);
+
         if (data.weather[0].main == 'Clouds') {
             weatherIcon.src = 'assets/images/cloudy.png';    
             newColor.style.backgroundColor = '#599CBA';
@@ -50,7 +78,7 @@ async function checkWeather(city) {
 function getInput() {
     /* Remove empty spaces before and after string(trim), 
     numbers and special characters (regex) before passing the input value to API */
-    cityName = ((searchBox.value).replace(/[0-9.,*+^<>?!$#$%&{}()|[\]\\]/g, '')).trim();
+    const cityName = ((searchBox.value).replace(/[0-9.,*+^<>?!$#$%&{}()|[\]\\]/g, '')).trim();
 
     /*Check if input is not empty and if it is not then pass the value to API, 
     if it is empty do nothing */
